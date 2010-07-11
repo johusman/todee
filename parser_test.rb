19fc3cb6347f90aec25182fd9c2ec751568cb415
Ztream <ztream@highrad.org>
2010-07-11 22:27:54 +0200

    Added source code parsing (forgot in last commit)

---
+++ parser_test.rb
@@ -0,0 +1,159 @@
+require 'todee_parser'
+
+describe TodeeParser, "parsing normal single code points" do
+  before do
+    @parser = TodeeParser.new
+  end
+  
+  it "should parse a simple ADD" do
+    cp, leftover = @parser.consume_code_point("add %1, 2, 3")
+    leftover.should == ""
+    cp.instruction_symbol.should == :ADD
+    cp.reference.should == 1
+    cp.arguments.map{|arg| arg.value }.should == [2, 3]
+    cp.arguments.map{|arg| arg.is_ref? }.should == [false, false]
+  end
+  
+  it "should parse an ADD with references" do
+    cp, leftover = @parser.consume_code_point("add %1, %2, %3")
+    leftover.should == ""
+    cp.instruction_symbol.should == :ADD
+    cp.reference.should == 1
+    cp.arguments.map{|arg| arg.value }.should == [2, 3]
+    cp.arguments.map{|arg| arg.is_ref? }.should == [true, true]
+  end
+  
+  it "should parse character arguments" do
+    cp, leftover = @parser.consume_code_point("add %1, 'h', 'j'")
+    leftover.should == ""
+    cp.instruction_symbol.should == :ADD
+    cp.reference.should == 1
+    cp.arguments.map{|arg| arg.value }.should == [104, 106]
+    cp.arguments.map{|arg| arg.is_ref? }.should == [false, false]
+  end
+
+  it "should return additional code points as leftovers" do
+    cp, leftover = @parser.consume_code_point("add %1, %2, %3 TUR 0")
+    leftover.strip.should == "TUR 0"
+  end
+  
+  it "should ignore non-syntax whitespace" do
+    cp, leftover = @parser.consume_code_point(" add  %1 ,  %2  , %3 ")
+    leftover.strip.should == ""
+    cp.instruction_symbol.should == :ADD
+    cp.reference.should == 1
+    cp.arguments.map{|arg| arg.value }.should == [2, 3]
+    cp.arguments.map{|arg| arg.is_ref? }.should == [true, true]
+  end
+  
+  it "should manage instructions with no target" do
+    cp, leftover = @parser.consume_code_point("TUR 4")
+    leftover.strip.should == ""
+    cp.instruction_symbol.should == :TUR
+    cp.reference.should == nil
+    cp.arguments.map{|arg| arg.value }.should == [4]
+    cp.arguments.map{|arg| arg.is_ref? }.should == [false]
+  end
+end
+
+describe TodeeParser, "parsing broken single code points" do
+  before do
+    @parser = TodeeParser.new
+  end
+  
+  def test_fail()
+    lambda { yield() }.should raise_error(TodeeParseException)
+  end
+  
+  it "should fail on too few arguments" do
+    #lambda { @parser.consume_code_point("add %1, 2 TUR 0") }.should raise_error(TodeeParseException)
+    
+    test_fail { @parser.consume_code_point("add %1, 2 TUR 0") }
+  end
+
+  it "should fail on too many arguments" do
+    test_fail { @parser.consume_code_point("add %1, 2, 3, 4 TUR 0") }
+  end
+  
+  it "should fail when target is a reference" do
+    test_fail { @parser.consume_code_point("add 1, 2 TUR 0") }
+  end
+
+  it "should fail on too many arguments to a target-less instruction" do
+    test_fail { @parser.consume_code_point("TUR %1, %2 TUR 0") }
+  end
+  
+  it "should not accept negative references" do
+    test_fail { @parser.consume_code_point("MOV %1, %-2 TUR 0") }
+  end
+  
+  it "should bark on missing arguments between commas" do
+    test_fail { @parser.consume_code_point("MOV %1,, %2 TUR 0") }
+  end  
+  
+  it "should not accept comma after op code" do
+    test_fail { @parser.consume_code_point("MOV, %1, %2 TUR 0") }
+  end    
+end
+
+describe TodeeParser, "parsing whole line" do
+  before do
+    @parser = TodeeParser.new
+  end
+  
+  it "should parse two simple code points" do
+    cp1, cp2 = @parser.parse_code_points("add %1, 2, 3\tmov %4, %1")
+    
+    cp1.instruction_symbol.should == :ADD
+    cp1.reference.should == 1
+    cp1.arguments.map{|arg| arg.value }.should == [2, 3]
+    cp1.arguments.map{|arg| arg.is_ref? }.should == [false, false]
+    
+    cp2.instruction_symbol.should == :MOV
+    cp2.reference.should == 4
+    cp2.arguments.map{|arg| arg.value }.should == [1]
+    cp2.arguments.map{|arg| arg.is_ref? }.should == [true]
+  end
+  
+  it "should ignore comments" do
+    cps = @parser.parse_code_points("add %1, 2, 3 # add some stuff")
+    cps.size.should == 1
+    
+    cps[0].instruction_symbol.should == :ADD
+    cps[0].reference.should == 1
+    cps[0].arguments.map{|arg| arg.value }.should == [2, 3]
+    cps[0].arguments.map{|arg| arg.is_ref? }.should == [false, false]
+    
+    @parser.parse_code_points("# just a comment").size.should == 0
+  end
+end
+
+describe TodeeParser, "parsing whole file" do
+  before do
+    @parser = TodeeParser.new
+  end
+  
+  it "should parse hello world" do
+    file = <<EOF
+      # A rather stupid Hello World program, provided the following context:
+      #  0 = stdout
+      #  1 = stack socket
+      #  2 = memory socket
+      mov %1, 10      # Single line feed for unix systems
+      mov %1, '!'
+      mov %1, 'o'
+      mov %1, 'l'
+      mov %1, 'l'
+      mov %1, 'e'
+      mov %1, 'h'
+      TUR 0           TNL 1
+      mov %2, %1      mov %0, %2
+      TNL %2          TNL 1
+      STP
+EOF
+    
+    matrix = @parser.parse_file(file)
+    matrix.size.should == 11
+    matrix.each { |row| row.size.should == 2 }
+  end
+end
---
+++ todee_parser.rb
@@ -0,0 +1,170 @@
+require 'todee_codepoint'
+require 'todee_code_utils'
+
+class TodeeParseException < Exception; end
+
+class TodeeParser
+  include CodeUtils
+  
+  def parse_file(file)
+    if not file.respond_to?("each_line") then
+      raise ArgumentError, "Unknown argument type #{file.type}. Must support each_line()"
+    end
+    
+    code = []
+    
+    file.each_line() do |line|
+      code_points = parse_code_points(line)
+      if code_points.size > 0 then
+        code << code_points
+      end
+    end
+    
+    max_cols = code.reduce(0) {|max,row| if row.size > max then row.size else max end }
+    code.each() do |row|
+      fill_size = max_cols - row.size
+      if fill_size > 0 then
+        filler = [CodePoint::NOP] * fill_size
+        row.push(*filler)
+      end
+    end
+    
+    return code
+  end
+
+  def parse_code_points(string)
+    code_points = []
+    until not string or string.strip.empty?
+      code_point, string = consume_code_point(string)
+      if code_point then
+        code_points << code_point
+      end
+    end
+    
+    return code_points
+  end
+  
+  def consume_code_point(string)
+    work_string = string.strip
+    if work_string.match(/^\s*#/) then
+      return [nil, nil] # line is a comment
+    end
+    
+    op, work_string = consume_op(work_string)
+    if not op then
+      raise_error "Expected instruction name at the beginning of '#{work_string}'", string
+    end
+    
+    op = op.upcase.strip.to_sym
+    
+    instruction = get_instruction(op)
+    
+    if not instruction then
+      raise_error "Instruction '#{op}' could not be found", string
+    end
+    
+    params, work_string = consume_parameters(work_string)
+    
+    target = nil
+    if instruction.takes_target then
+      target_param = params.shift
+      if not target_param then
+        raise_error "Instruction '#{op}' takes a target argument, but there were no arguments given", string
+      end
+      target = parse_param(target_param) 
+      if not target then
+        raise_error "Argument #{target_param} is not a valid argument (should be [%]<number> or '<character>')", string
+      end
+      if target and not target.is_ref? then
+        raise_error "First argument to instruction '#{op}' is a target and must therefore be a reference, but was: '#{target_param}'", string
+      end
+    end
+    
+    if params.size != instruction.num_args then
+      raise_error "Instruction '#{op}' takes #{instruction.num_args + (instruction.takes_target ? 1 : 0)} arguments, but was given #{params.size + (target ? 1 : 0)}", string
+    end
+    
+    args = params.map do |param|
+      parsed = parse_param(param)
+      if not parsed then
+        raise_error "Argument #{param} is not a valid argument (should be [%]<number> or '<character>')", string
+      else
+        parsed
+      end
+    end
+    
+    return [CodePoint.new(op, target ? target.value : nil, args), work_string]
+  end
+private
+
+  def raise_error(message, string)
+    error_message = "#{message} (while parsing '#{string.strip}')"
+    raise TodeeParseException, error_message
+  end
+
+  def consume_op(string)
+    match = string.match(/^(\w+)\s*(.*)/)
+    if match then
+      return [match[1], match[2]]
+    else
+      return [nil, string]
+    end
+  end
+  
+  def consume_parameter(string)
+    #match = string.match(/^\s*(((%|-)?[0-9]+)|('[^']?'))(.*)/)
+    match = string.match(/^\s*(([%\-0-9]+)|('[^']?'))(.*)/)
+    if match then
+      return [match[1], match[4]]
+    else
+      return [nil, string]
+    end
+  end
+  
+  def consume_parameters(string)
+    params = []
+    work_string = string
+    while true
+      param, work_string = consume_parameter(work_string)
+      if param
+	params << param
+	delim_match = work_string.match(/^\s*,\s*(.*)/)
+	if delim_match
+	  work_string = delim_match[1]
+	else
+	  return [params, work_string]
+	end
+      else
+	return [params, work_string]
+      end 
+    end
+  end
+  
+  def parse_param(param)
+    param = param.strip
+    
+    if not param.match(/^((%|-)?[0-9]+)|('[^']?')$/) then
+      return nil
+    end
+    
+    is_ref = false
+    ref_match = param.match(/^%(.*)/)
+    if ref_match then
+      is_ref = true
+      param = ref_match[1]
+    end
+    
+    quote_match = param.match(/^'([^']?)'$/)
+    if quote_match then
+      if quote_match[1] and not quote_match[1].empty? then
+        param = quote_match[1][0].to_s
+      else
+        param = "'"[0].to_s
+      end
+    end
+    
+    return nil if not param.match(/^-?[0-9]+$/)
+    
+    return Argument.new(param.to_i, is_ref)
+  end
+end
